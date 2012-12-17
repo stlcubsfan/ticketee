2d06474048fa4e05140c06fe6fb99d95d8975a09
Brian Coles <stlcubsfan@gmail.com>
2012-12-17 11:02:04 -0600

    Finished initial project create and tests pass

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,4 +6,18 @@ class ProjectsController < ApplicationController
   def new
     @project = Project.new
   end
+
+  def create
+  	@project = Project.new(params[:project])
+  	if @project.save
+  		flash[:notice] = "Project has been created"
+  		redirect_to @project
+  	else
+
+  	end
+  end
+
+  def show
+  	@project = Project.find(params[:id])
+  end
 end
