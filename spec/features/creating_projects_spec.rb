3be91d662f8099e760774b82b9760fd918cbf4a1
Brian Coles <stlcubsfan@gmail.com>
2012-12-19 20:53:18 -0600

    Adding validation on project names

--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -1,9 +1,11 @@
 require 'spec_helper'
 
 feature 'Creating Projects' do
+  before do
+ 		visit '/'
+ 		click_link 'New Project'
+  end
   scenario 'can create a project' do
-    visit '/'
-    click_link 'New Project'
     fill_in 'Name', :with => 'TextMate 2'
     fill_in 'Description', :with => 'A text-editor for OS X'
     click_button 'Create Project'
@@ -12,5 +14,11 @@ feature 'Creating Projects' do
     project = Project.find_by_name("TextMate 2")
     page.current_url.should == project_url(project)
 
+  end
+
+  scenario 'can not create a project without a name' do
+  	click_button 'Create Project'
+  	page.should have_content("Project has not been created")
+  	page.should have_content("Name can't be blank")
   end
 end
