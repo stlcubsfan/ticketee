7bd39768b8ff785217c14c836f58b073f46ce68c
Brian Coles <stlcubsfan@gmail.com>
2012-12-19 20:40:25 -0600

    Added title helper

--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -8,5 +8,9 @@ feature 'Creating Projects' do
     fill_in 'Description', :with => 'A text-editor for OS X'
     click_button 'Create Project'
     page.should have_content('Project has been created')
+
+    project = Project.find_by_name("TextMate 2")
+    page.current_url.should == project_url(project)
+
   end
 end
